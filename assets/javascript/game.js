6605f76188f459b98942d7778e6b71e24f7a1aa8
Andrew Ulrich <andyinthemachine@hotmail.com>
2019-05-30 21:15:55 -0600

    visual layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+
+
+var wins = 0;
+var losses = 0;
+var guesses_left = 0;
+var guesses = "";
+
+
+var wins_display = document.getElementById("wins");
+var losses_display = document.getElementById("losses");
+var guesses_left_display = document.getElementById("guesses_left");
+var guesses_display = document.getElementById("guesses");
+
+wins_display.textContent = wins;
+losses_display.textContent = losses;
+guesses_left_display.textContent = guesses_left;
+guesses_display.textContent = guesses;
+
+// JavaScript function onkeyup event
+// document.onkeyup = function(event) 
+// {
+//   //userText.textContent = event.key;
+//   user_play = event.key;
+//   userText.textContent = user_play;
+//   if ((event.key === 'r') || (event.key === 'p') || (event.key === 's')) 
+//   {
+//     comp_play = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+//     computerText.textContent = comp_play;
+
+//     if (user_play === comp_play)
+//       ties++
+//     else if (((user_play === 'r') && (comp_play === 's')) ||
+//              ((user_play === 'p') && (comp_play === 'r')) ||
+//              ((user_play === 's') && (comp_play === 'p')))
+//     {
+//       wins++;
+//     }
+//     else 
+//       losses++;
+    
+//     winsText.textContent = wins;
+//     lossesText.textContent = losses;
+//     tiesText.textContent = ties;
+//   }
+// }
