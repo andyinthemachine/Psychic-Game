415a4c08e03d860e5c00edfef80e20a76111fb14
Andrew Ulrich <andyinthemachine@hotmail.com>
2019-05-31 22:31:56 -0600

    logic functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 
-
+var random_letter = "";
 var wins = 0;
 var losses = 0;
-var guesses_left = 0;
+var guesses_left = 9;
 var guesses = "";
 
-
+var random_letter_display = document.getElementById("random_letter");
 var wins_display = document.getElementById("wins");
 var losses_display = document.getElementById("losses");
 var guesses_left_display = document.getElementById("guesses_left");
@@ -13,41 +13,48 @@ var guesses_display = document.getElementById("guesses");
 
 function update_display()
 {
-    wins_display.textContent = wins;
-    losses_display.textContent = losses;
-    guesses_left_display.textContent = guesses_left;
-    guesses_display.textContent = guesses;
+  random_letter_display.textContent = random_letter;
+  wins_display.textContent = wins;
+  losses_display.textContent = losses;
+  guesses_left_display.textContent = guesses_left;
+  guesses_display.textContent = guesses;
 }
 
-update_display();
+function reset_game(num_guesses)
+{
+  guesses_left = num_guesses;
+  guesses = "";
+}
+
+
+update_display();  
 
 // JavaScript function onkeyup event
 document.onkeyup = function(event) 
 {
   user_play = event.key;
-  guesses = guesses  + user_play + ', ';
+  guesses = guesses  + user_play;
+  if (guesses_left > 2)
+    guesses = guesses + ', ';
+
+  // sample from Unicode values representing lower case alphabet letters
+  random_letter = String.fromCharCode(97 + (Math.floor(Math.random() * 26)));
   
-  comp_play = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  if (random_letter === user_play)  // a win
+  {
+    wins++;
+    reset_game(9);
+  }
+  else if (guesses_left-- <= 1)
+  {
+    losses++;
+    reset_game(9);
+  }
 
-
-  update_display();
-
-//   if ((event.key === 'r') || (event.key === 'p') || (event.key === 's')) 
-//   {
-//     comp_play = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-//     computerText.textContent = comp_play;
-
-//     if (user_play === comp_play)
-//       ties++
-//     else if (((user_play === 'r') && (comp_play === 's')) ||
-//              ((user_play === 'p') && (comp_play === 'r')) ||
-//              ((user_play === 's') && (comp_play === 'p')))
-//     {
-//       wins++;
-//     }
-//     else 
-//       losses++;
-    
-   
- 
+  update_display();  
 }
+
+  
+
+
+    
